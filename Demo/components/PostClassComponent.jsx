ba2364ff50fe2f868dfae1f9015ff2263d29ded6
lNewED <NewHaZao.4@gmail.com>
2023-09-01 21:16:48 +0700

    first commit

---
+++ Demo/components/PostClassComponent.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+import "../Post.css"
+
+export default class PostClassComponent extends Component{
+    render() {
+        return(
+            <div className="post">
+                <p><strong>{this.props.userId}</strong> 
+                : {this.props.message}</p>
+                <p>Comments:</p> 
+                <p>{this.props.children}</p>
+            </div>
+        );
+    }
+}
---
+++ Demo/components/PostFunctionComponent.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PostFunctionComponent = () => {
+    return(
+        <div>
+            <h2 style={{color: "red"}}> This is a function Component </h2>
+        </div>
+    );
+}
+
+export default PostFunctionComponent
